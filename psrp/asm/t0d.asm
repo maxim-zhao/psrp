d60644a6a1564a63ac09d72d50060f696c8a0e3e
maximzhao <maximzhao@38fa3542-410f-0410-9225-c8cbef270411>
2006-11-01 13:42:43 +0000

    Working on documenting ROM usage for code additions

--- psrp/asm/t0d.asm
+++ psrp/asm/t0d.asm
@@ -4,45 +4,113 @@
 ; Written in TASM 3.0.1
 ;
 
-.org $34f2			; $34f2-3545
+.org $34f2
+; Replacing draw-one-character function from $34f2-3545
+; drawing 2 tilemap chars, with conditional mirroring, and a scrolling handler,
+; with a new one from $3f42-3f66 that draws a single char using a 2-byte lookup
+; and leaves the scrolling to be handled by the word-wrapping code elsewhere
 
-	DI			; Prepare VRAM output
-	PUSH BC
-	PUSH DE
+  di            ; prepare VRAM output
+  push bc
+    push de     
+                
+      push af
+        rst 08h      ; Set address
+      pop af
 
-	PUSH AF
-	RST 08H			; Set address
-	POP AF
+      ; ld a,(hl)    ; A = decoded byte
+      add a,a        ; 2-byte table
 
-	; LD A,(HL)		; A = decoded byte
-	ADD A,A			; 2-byte table
+      ld bc,$8000    ; index into table
+      add a,c
+      ld c,a
 
-	LD BC,$8000		; index into table
-	ADD A,C
-	LD C,A
+      adc a,b       ; overflow accounting
+      sub c
+      ld b,a
 
-	ADC A,B			; overflow accounting
-	SUB C
-	LD B,A
+      ld a,(bc)     ; load low NT-byte
+      out ($be),a
+      push af       ; VRAM wait
+      pop af
 
-	LD A,(BC)		; load low NT-byte
-	OUT ($BE),A
-	PUSH AF			; VRAM wait
-	POP AF
+      inc bc        ; write out high NT-byte
+      ld a,(bc)
+      out ($be),a
 
-	INC BC			; write out high NT-byte
-	LD A,(BC)
-	OUT ($BE),A
+    pop de        ; Bump VRAM address
+    inc de
+    inc de
+  pop bc
 
-	POP DE			; Bump VRAM address
-	INC DE
-	INC DE
-	POP BC
+  ei               ; Wait for VBlank
+  ld a,$0a
+  call $0056    
+                
+  dec b             ; Shrink window width
+  ret
 
-	EI			; Wait for VBlank
-	LD A,$0A
-	CALL $0056
+; Original:
+;_DrawLetter:           ; $34f2
+;    di
+;    push bc
+;      push de
+;        SetVRAMAddressToDE
+;        ld a,(hl)      ; Get data (upper half of letter)
+;        add a,a
+;        ld bc,TileNumberLookup
+;        add a,c
+;        ld c,a
+;        adc a,b
+;        sub c
+;        ld b,a         ; bc = TileNumberLookup + 2*a
+;        ld a,(bc)      ; get tile number corresponding to a from there
+;        out (VDPData),a
+;        push af        ; delay
+;        pop af
+;        ld a,$10       ; in front of sprites
+;        out (VDPData),a
+;        ex de,hl
+;        ld bc,$40      ; add $40 to de (1 row)
+;        add hl,bc
+;        ex de,hl
+;        SetVRAMAddressToDE
+;        ld a,(hl)      ; get data (lower half of letter)
+;        add a,a
+;        ld bc,TileNumberLookup+1
+;        add a,c
+;        ld c,a
+;        adc a,b
+;        sub c
+;        ld b,a         ; bc = TileNumberLookup+1 + 2*a
+;        ld a,(bc)      ; get tile number corresponding to a from there
+;        out (VDPData),a
+;        push af        ; delay
+;        pop af
+;        cp $fe         ; should the lower half be reversed? (tile $fe = . also acts as a dot above a letter the other way round)
+;        ld a,$12       ; if so, in front of sprite + horizontal mirror
+;        jr z,+
+;        ld a,$10       ; else in front of sprites
+;      +:out (VDPData),a
+;        inc hl         ; move to next data
+;      pop de
+;      inc de
+;      inc de
+;    pop bc
+;    ei
+;    ld a,$0a           ; VBlankFunction_Enemy (I have to fix these labels...)
+;    call ExecuteFunctionIndexAInNextVBlank
+;    dec b              ; dec b
+;    ret nz             ; return if non-zero -> only do next bit when b=1 at start
+;
+;    ld a,(hl)          ; peek at next data
+;    cp $53
+;    ret nc             ; if >=$53 (control codes)
+;    ld a,c             ; get c
+;    or a               ; if non-zero,
+;    call nz,_ScrollTextBoxUp2Lines
+;    ld de,$7d4e        ; TileMapAddressDE 7,21
+;    ld bc,$1201        ; reset b=1, c=$12
+;    ret
 
-	DEC B			; Shrink window width
-	RET
-.end
+  .end      ; TASM-only
