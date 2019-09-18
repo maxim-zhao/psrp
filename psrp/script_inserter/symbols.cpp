a4691d975fb15e2c97118f098f21acc183959e1a
Maxim <maxim@smspower.org>
2019-09-18 20:02:09 +0100

    Removing unused scripting features, converting bool-ish ints to bools

--- psrp/script_inserter/symbols.cpp
+++ psrp/script_inserter/symbols.cpp
@@ -3,7 +3,6 @@ Phantasy Star: Symbol Converter (Script)
 */
 
 
-#include <cwchar>
 #include <cstdio>
 #include <cstring>
 
@@ -16,108 +15,112 @@ Phantasy Star: Symbol Converter (Script)
 
 namespace
 {
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> convert;
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> convert;
 }
 
 class File
 {
-    std::ifstream _f;
+	std::ifstream _f;
 	int _lineNumber;
 public:
-    explicit File(const std::string& name): _f(name), _lineNumber(0)
-    {
-        // Check for a BOM
-        int bom = 0;
-        _f.read(reinterpret_cast<char *>(&bom), 3);
-        if (bom != 0xbfbbef)
-        {
-            // Not found -> rewind
-            _f.seekg(0);
-        }
-    }
-    bool getLine(std::string& s)
-    {
-        for (;;)
-        {
-            getline(_f, s);
-            if (_f.fail())
-            {
-                return false;
-            }
-        	++_lineNumber;
-            // Remove comments
-            const auto pos = s.find(';');
-            if (pos != std::string::npos)
-            {
-                s.erase(pos);
-            }
-            // Discard empty lines
-            if (s.empty())
-            {
-                continue;
-            }
-            return true;
-        }
-    }
+	explicit File(const std::string& name): _f(name), _lineNumber(0)
+	{
+		// Check for a BOM
+		int bom = 0;
+		_f.read(reinterpret_cast<char *>(&bom), 3);
+		if (bom != 0xbfbbef)
+		{
+			// Not found -> rewind
+			_f.seekg(0);
+		}
+	}
+
+	bool getLine(std::string& s)
+	{
+		for (;;)
+		{
+			getline(_f, s);
+			if (_f.fail())
+			{
+				return false;
+			}
+			++_lineNumber;
+			// Remove comments
+			const auto pos = s.find(';');
+			if (pos != std::string::npos)
+			{
+				s.erase(pos);
+			}
+			// Discard empty lines
+			if (s.empty())
+			{
+				continue;
+			}
+			return true;
+		}
+	}
 
 	int lineNumber() const { return _lineNumber; }
 };
 
 class Table
 {
-    std::map<wchar_t, int> _table;
+	std::map<wchar_t, int> _table;
 
 public:
-    explicit Table(const std::string& fileName)
-    {
-        File f(fileName);
+	explicit Table(const std::string& fileName)
+	{
+		File f(fileName);
 
-        for (std::string s; f.getLine(s);)
-        {
-            // Remove comments
-            const auto pos = s.find(';');
-            if (pos != std::string::npos)
-            {
-                s.erase(pos);
-            }
-            // Discard empty lines
-            if (s.empty())
-            {
-                continue;
-            }
-            // Find the "="
-            const auto equalsPos = s.find('=');
-            if (equalsPos == std::string::npos)
-            {
-                printf("Warning: unexpected table line \"%s\"\n", s.c_str());
-                continue;
-            }
-            // Parse the bits
-            const auto& l = s.substr(0, equalsPos);
-            const auto& r = s.substr(equalsPos + 1);
-            // l is little-endian hex...
-            int value = std::stoi(l, nullptr, 16);
-            if (l.length() == 4)
-            {
-                // swap bytes
-                value = (value & 0xff) << 8 | (value >> 8);
-            }
-            // r is possibly UTF-8...
-            std::wstring dest = convert.from_bytes(r.c_str());
-            // We assume we only want the first char
-            _table[dest[0]] = value;
-        }
-    }
+		for (std::string s; f.getLine(s);)
+		{
+			// Remove comments
+			const auto pos = s.find(';');
+			if (pos != std::string::npos)
+			{
+				s.erase(pos);
+			}
+			// Discard empty lines
+			if (s.empty())
+			{
+				continue;
+			}
+			// Find the "="
+			const auto equalsPos = s.find('=');
+			if (equalsPos == std::string::npos)
+			{
+				printf("Warning: unexpected table line \"%s\"\n", s.c_str());
+				continue;
+			}
+			// Parse the bits
+			const auto& l = s.substr(0, equalsPos);
+			const auto& r = s.substr(equalsPos + 1);
+			// l is little-endian hex...
+			int value = std::stoi(l, nullptr, 16);
+			if (l.length() == 4)
+			{
+				// swap bytes
+				value = (value & 0xff) << 8 | (value >> 8);
+			}
+			// r is possibly UTF-8...
+			std::wstring dest = convert.from_bytes(r.c_str());
+			// We assume we only want the first char
+			_table[dest[0]] = value;
+		}
+	}
 
-    int find(wchar_t value)
-    {
-        const auto it = _table.find(value);
-        if (it == _table.end())
-        {
-            return -1;
-        }
-        return it->second;
-    }
+	int find(wchar_t value)
+	{
+		const auto it = _table.find(value);
+		if (it == _table.end())
+		{
+			return -1;
+		}
+		return it->second;
+	}
+
+	// TODO: implement "find longest match at start of string"
+	// Needs to return the match and also the length?
 };
 
 #define EOS 0x56
@@ -222,24 +225,16 @@ void Load_Tables(int direction, const std::string& filename)
 #define old_symbol old_symbol_table[ start ][ entry ]
 #define new_symbol new_symbol_table[ start ][ entry ]
 
-int script_width;
-int script_height;
+int script_width; // Text box width, used for wrapping
+int script_height; // Text box height, not used
 
-int script_center;
-int script_wrap;
-int script_border;
-int script_end;
-int script_line;
+bool script_end; // Set to true when reaching a line-ending command
 
-int script_quote;
-int script_autoquote;
-int script_autowait;
-int script_hints;
-int script_intro;
+bool script_hints; // Set to true when a "hint" is active (?)
 
-int script_internal_hint;
+int script_internal_hint; // "hint" value
 
-int line_len;
+int line_len; // Line length, used for wrapping
 
 void Scan_Text(const char*& pText, int& width)
 {
@@ -271,7 +266,7 @@ void Scan_Text(const char*& pText, int& width)
 		pass1.put( 0x59); \
 		pass1.put( post_hint); \
 	} \
-	script_hints = 1;
+	script_hints = true;
 
 
 void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
@@ -336,95 +331,23 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 			break;
 
 			// internal text wrapping
-		case 1: script_wrap = 1;
-			break;
-
-			// no internal text wrapping
-		case 2: script_wrap = 0;
-			break;
-
-			// internal text centering
-		case 3: script_center = 1;
-			break;
-
+		case 1:
 			// no internal text centering
-		case 4: script_center = 0;
+		case 4:
+			// always on, swallow code
 			break;
-
-			// scroll code
-		case 5:
-			{
-				int value;
-
-				sscanf(pText, "%02X", &value);
-				pText += 2;
-
-				pass1.put(0x01);
-				pass1.put(value);
-			}
-			break;
-
-			// # newlines
-		case 6:
-			{
-				int value;
-
-				sscanf(pText, "%02X", &value);
-				pText += 2;
-
-				pass1.put(0x02);
-				pass1.put(value);
-			}
-			break;
-
-			// internal border marker
-		case 7:
-			sscanf(pText, "%02X", &script_border);
-			pText += 2;
-			break;
-
-			// used to unroll substring matches
-		case 8:
-			{
-				std::string str;
-
-				// tack on addition
-				str = '>';
-				while (*pText != '>') str += *(pText++);
-				str += (pText + 1);
-				//memcpy(pText, str.c_str(), str.length());
-			}
-			break;
-
-			// start quote formatting
-		case 9: script_autoquote = 1;
-			script_border++;
-			break;
-
-			// end quote formatting
-		case 10: script_autoquote = 0;
-			script_border--;
-			break;
-
-			// start intro formatting
-		case 11: script_intro = 1;
-			break;
-
-			// end intro formatting
-		case 12: script_intro = 0;
-			break;
-
+			
 			// internal script hint (for suffix spillage prevention)
 		case 13:
 			{
 				sscanf(pText, "%02X", &script_internal_hint);
 				pText += 2;
 
-				script_hints = 1;
+				script_hints = true;
 			}
 			break;
 
-			// internal width check
+			// internal height check
 		case 14:
 			sscanf(pText, "%02X", &script_height);
 			pText += 2;
@@ -455,24 +378,11 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 			// add plain newline
 		case 19:
 			{
-				script_line++;
-				if (script_line % script_height == 0)
-				{
-					// wait more
-					if (script_autowait) pass1.put(0x55);
-
-					// use alternate delay
-					if (script_intro) pass1.put(0x57);
-				}
-
-				// clear line counter (handled via engine)
-				//if( script_intro && script_line % script_height == 0 ) fputc( 0xce, pass1 );
-
 				// add newline
 				pass1.put(0x54);
 
 				line_len = 0;
-				script_hints = 0;
+				script_hints = false;
 			}
 			break;
 
@@ -481,8 +391,7 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 			{
 				pass1.put(0x55);
 
-				script_line = 0;
-				script_hints = 0;
+				script_hints = false;
 				line_len = 0;
 			}
 			break;
@@ -493,9 +402,8 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 				pass1.put(0x56);
 
 				// de-init
-				script_end = 1;
-				script_line = 0;
-				script_hints = 0;
+				script_end = true;
+				script_hints = false;
 				line_len = 0;
 			}
 			break;
@@ -507,9 +415,8 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 				pass1.put(0x56);
 
 				// de-init
-				script_end = 1;
-				script_line = 0;
-				script_hints = 0;
+				script_end = true;
+				script_hints = false;
 				line_len = 0;
 			}
 			break;
@@ -521,9 +428,8 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 				pass1.put(0x56);
 
 				// de-init
-				script_end = 1;
-				script_line = 0;
-				script_hints = 0;
+				script_end = true;
+				script_hints = false;
 				line_len = 0;
 			}
 			break;
@@ -539,7 +445,7 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 				pass1.put(0x5a);
 				pass1.put(value);
 
-				script_hints = 1;
+				script_hints = true;
 			}
 			break;
 
@@ -548,8 +454,12 @@ void Process_Code(const char* & pText, std::ostream& pass1, int line_num)
 			{
 				pass1.put(0x5b);
 
-				script_hints = 1;
+				script_hints = true;
 			}
+			break;
+		default:
+			printf("(line %d) ERROR: Script code removed '%s'\n",
+			       line_num, read_symbol.c_str());
 			break;
 		}; // end switch
 	}
@@ -622,22 +532,15 @@ void Find_Entry(const char*& pText, int& index, int line_num)
 void Process_Text(const std::string& name, std::ostream& pass1, const Table& table)
 {
 	File f(name);
-	
+
 	// Init
 	script_width = 0;
-	script_center = 0;
-	script_line = 0;
-	script_autowait = 0;
-	script_quote = 0;
-	script_autoquote = 0;
-	script_border = 0;
-	script_hints = 0;
-	script_intro = 0;
+	script_hints = false;
 	script_internal_hint = 0;
-	script_end = 0;
+	script_end = false;
 
 	// Read in string entries
-	for (std::string s; f.getLine(s); )
+	for (std::string s; f.getLine(s);)
 	{
 		const char* pText;
 		std::vector<uint8_t> out_buffer;
@@ -653,7 +556,7 @@ void Process_Text(const std::string& name, std::ostream& pass1, const Table& tab
 		}
 
 		// internal counter
-		script_end = 0;
+		script_end = false;
 
 		// do the conversion
 		while (*pText)
@@ -679,83 +582,66 @@ void Process_Text(const std::string& name, std::ostream& pass1, const Table& tab
 				}
 
 				Process_Code(pText, pass1, f.lineNumber());
-				if (script_end) break;
+				if (script_end) 
+				{
+					break;
+				}
 				continue;
 			}
 
 			// check if we have a dictionary entry
 			Find_Entry(pText, entry, f.lineNumber());
-			if (entry == -1) continue;
+			if (entry == -1) 
+			{
+				continue;
+			}
 
 			// attempt auto-formatting
-			if (script_wrap)
+			// check for whitespace
+			if (start == ' ')
 			{
-				// check for whitespace
-				if (start == ' ')
+				int width;
+				const char* pTmp = pOld + 1;
+
+				// flush data
+				if (!out_buffer.empty())
 				{
-					int width;
-					const char* pTmp = pOld + 1;
+					pass1.write((const char*)&out_buffer[0], out_buffer.size());
 
-					// flush data
-					if (!out_buffer.empty())
+					// reset
+					out_buffer.clear();
+				}
+
+				// scan for next non-text moment
+				Scan_Text(pTmp, width);
+
+				// see if next word does not fit in this same line
+				if (line_len + 1 + width > script_width && !script_hints)
+				{
+					// add newline
+					pass1.put(0x54);
+
+					// reset x-pos
+					line_len = 0;
+
+					// bypass only whitespace
+					pText = pOld + 1;
+
+					continue;
+				}
+				else
+				{
+					// real-time line formatting needed
+					if (script_hints && width)
 					{
-						pass1.write((const char*)&out_buffer[0], out_buffer.size());
+						pass1.put(0x59);
+						pass1.put(width + script_internal_hint);
 
-						// reset
-						out_buffer.clear();
+						// manual hint flag reset
+						script_internal_hint = 0;
 					}
-
-					// scan for next non-text moment
-					Scan_Text(pTmp, width);
-
-					// see if next word does not fit in this same line
-					if (line_len + 1 + width + script_border * 2 > script_width &&
-						(!script_autoquote || *(pTmp - 1) != '\"') && !script_hints)
-					{
-						script_line++;
-						if (script_line % script_height == 0)
-						{
-							// use 'wait more'
-							if (script_autowait) pass1.put(0x55);
-
-							// use alternate delay
-							if (script_intro) pass1.put(0x57);
-
-							// reset y-pos
-							script_line = 0;
-						}
-
-						// add newline
-						pass1.put(0x54);
-
-						// reset x-pos
-						line_len = 0;
-
-						// bypass only whitespace
-						pText = pOld + 1;
-
-						// add padding for quotes
-						if (script_autoquote)
-						{
-							pass1.put(0x00);
-							line_len++;
-						}
-						continue;
-					}
-					else
-					{
-						// real-time line formatting needed
-						if (script_hints && width)
-						{
-							pass1.put(0x59);
-							pass1.put(width + script_internal_hint);
-
-							// manual hint flag reset
-							script_internal_hint = 0;
-						}
-					}
-				} // end whitespace
-			} // end script wrap
+				}
+			} // end whitespace
 
 			// successful -> start logging changes
 			for (int lcv2 = 0; lcv2 < new_symbol.length(); lcv2 += 2)
@@ -764,17 +650,6 @@ void Process_Text(const std::string& name, std::ostream& pass1, const Table& tab
 
 				// calculate new hex code
 				sscanf(new_symbol.c_str() + lcv2, "%02X", &code);
-
-#if 0
-				if( old_symbol == "\"" ) {
-					// end quote check
-					if( script_quote ) {
-						script_quote = 0;
-						code++;
-					} else
-						script_quote = 1;
-				}
-#endif
 
 				// buffer out data
 				out_buffer.push_back(code);
