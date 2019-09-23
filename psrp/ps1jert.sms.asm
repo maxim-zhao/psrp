cd77a441e88e83b155202415b924774de618a0d1
Maxim <maxim@smspower.org>
2019-09-23 20:04:49 +0100

    Moved credits into source code (WLA DX can do everything we need)

--- psrp/ps1jert.sms.asm
+++ psrp/ps1jert.sms.asm
@@ -3012,7 +3012,89 @@ WriteLetterToTileMapDataAndVRAM: ; $42b5
 
 ; Changed credits -------------------------
   ROMPosition $53dbc
-  Bin CreditsData "credits/credits.bin" force ; not movable?
+.section "Credits" force ; not movable?
+CreditsData:
+.dw CreditsScreen1, CreditsScreen2, CreditsScreen3, CreditsScreen4, CreditsScreen5, CreditsScreen6, CreditsScreen7, CreditsScreen8, CreditsScreen9, CreditsScreen10, CreditsScreen11, CreditsScreen12, CreditsScreen13, CreditsScreen14
+.macro CreditsEntry args x, y, text
+.dw $d000 + ((y * 32) + x) * 2
+.db text.length, text
+.endm
+CreditsScreen1:
+.db 1
+  CreditsEntry 13,10,"STAFF"
+CreditsScreen2:
+.db 3
+  CreditsEntry 5,5,"TOTAL"
+  CreditsEntry 6,7,"PLANNING"
+  CreditsEntry 17,6,"OSSALE KOHTA"
+CreditsScreen3:
+.db 5
+  CreditsEntry 6,5,"SCENARIO"
+  CreditsEntry 7,7,"WRITER"
+  CreditsEntry 17,6,"OSSALE KOHTA"
+  CreditsEntry 9,15,"STORY"
+  CreditsEntry 17,15,"APRIL FOOL"
+CreditsScreen4:
+.db 4
+  CreditsEntry 4,5,"ASSISTANT"
+  CreditsEntry 3,7,"COORDINATORS"
+  CreditsEntry 10,11,"OTEGAMI CHIE"
+  CreditsEntry 18,15,"GAMER MIKI"
+CreditsScreen5:
+.db 5
+  CreditsEntry 3,6,"TOTAL DESIGN"
+  CreditsEntry 18,6,"PHOENIX RIE"
+  CreditsEntry 5,14,"MONSTER"
+  CreditsEntry 7,16,"DESIGN"
+  CreditsEntry 17,15,"CHAOTIC KAZ"
+CreditsScreen6:
+.db 3
+  CreditsEntry 8,6,"DESIGN"
+  CreditsEntry 9,10,"ROCKHY NAO"
+  CreditsEntry 17,15,"SADAMORIAN"
+CreditsScreen7:
+.db 4
+  CreditsEntry 8,6,"DESIGN"
+  CreditsEntry 9,10,"MYAU CHOKO"
+  CreditsEntry 17,15,"G CHIE"
+  CreditsEntry 9,19,"YONESAN"
+CreditsScreen8:
+.db 4
+  CreditsEntry 9,6,"SOUND"
+  CreditsEntry 18,6,"BO"
+  CreditsEntry 4,15,"SOFT CHECK"
+  CreditsEntry 18,15,"WORKS NISHI"
+CreditsScreen9:
+.db 5
+  CreditsEntry 3,5,"ASSISTANT"
+  CreditsEntry 4,7,"PROGRAMMERS"
+  CreditsEntry 9,10,"COM BLUE"
+  CreditsEntry 4,15,"M WAKA"
+  CreditsEntry 19,15,"ASI"
+CreditsScreen10:
+.db 2
+  CreditsEntry 2,6,"MAIN PROGRAM"
+  CreditsEntry 17,6,"MUUUU YUJI"
+CreditsScreen11:
+.db 1
+  CreditsEntry 9,10,"RETRANSLATION"
+CreditsScreen12:
+.db 4
+  CreditsEntry 3,6,"WORDS"
+  CreditsEntry 10,10,"PAUL JENSEN"
+  CreditsEntry 2,15,"FRANK CIFALDI"
+  CreditsEntry 18,15,"SATSU"
+CreditsScreen13:
+.db 3
+  CreditsEntry 6,6,"CODE"
+  CreditsEntry 11,10,"Z[\ GAIDEN"
+  CreditsEntry 9,15,"MAXIM"
+CreditsScreen14:
+.db 3
+  CreditsEntry 10,10,"PRESENTED BY"
+  CreditsEntry 10,15,"SEGA"
+  CreditsEntry 18,15,"SMS POWER"
+.ends
 
   ROMPosition $488c
 .section "Credits hack" overwrite
@@ -3079,7 +3161,9 @@ PauseFMToggle:
 
 ; Bug matching: throwaway patch offsets went here
   ROMPosition $7fee
+.section "Silly bug" overwrite
 .dw $0c18
+.ends
 
 ; checksum (needs changing whenever anything changes)
   PatchW $7ffa $d6ac
