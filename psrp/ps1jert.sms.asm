f022fd184c6740b31b80d78a651bb38d59f79ab4
Maxim <maxim@smspower.org>
2021-01-16 00:38:06 +0000

    Added accented characters to pt-br name entry screen

--- psrp/ps1jert.sms.asm
+++ psrp/ps1jert.sms.asm
@@ -3784,31 +3784,31 @@ NameEntryLookup:
 .define NameEntryMaxY 19
 .endif
 .if LANGUAGE == "pt-br"
-.db 13
+.db 14
   NameEntryText  8,  1,      "Digite seu nome"
   NameEntryText  3, 11, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   NameEntryText  3, 13, "abcdefghijklmnopqrstuvwxyz"
-  ; TODO-pt-br add accented here once font is decided
-  NameEntryText  3, 15, "0123456789"
-  NameEntryText 21, 15,                   ".,-!?‘’"
-  NameEntryText  3, 17, "Voltar"
-  NameEntryText 22, 17,                    "Próximo"
-  NameEntryText  3, 19, "Espaço"
-  NameEntryText 23, 19,                     "Salvar"
+  NameEntryText  3, 15, "ãáàâçêéíóõôú"
+  NameEntryText  3, 17, "0123456789"
+  NameEntryText 22, 17,                   ".,-!?‘’"
+  NameEntryText  3, 19, "Voltar"
+  NameEntryText 22, 19,                    "Próximo"
+  NameEntryText  3, 21, "Espaço"
+  NameEntryText 23, 21,                     "Salvar"
   NameEntryText  1,  3, "┌─" ; Leave these ones alone...
   NameEntryText  1, 23, "╘═"
   NameEntryText 30,  3, "╖"
   NameEntryText 30, 23, "╝"
 NameEntryLookup:
 .db 4
-  NameEntryMask  3, 17, 6, "B" ; X, Y, length, type (Back)
-  NameEntryMask 22, 17, 7, "N" ; Next
-  NameEntryMask  3, 19, 6, "S" ; Space
-  NameEntryMask 23, 19, 6, "V" ; saVe
+  NameEntryMask  3, 19, 6, "B" ; X, Y, length, type (Back)
+  NameEntryMask 22, 19, 7, "N" ; Next
+  NameEntryMask  3, 21, 6, "S" ; Space
+  NameEntryMask 23, 21, 6, "V" ; saVe
 .define NameEntryMinX 3
 .define NameEntryMaxX 28
 .define NameEntryMinY 11
-.define NameEntryMaxY 19
+.define NameEntryMaxY 21
 .endif
 
 _CursorMemoryInitialValues:
