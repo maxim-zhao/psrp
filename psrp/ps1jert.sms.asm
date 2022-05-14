05423494e66b6278e32d76b73b4b50403bd1858c
Maxim <maxim@smspower.org>
2022-05-14 22:34:05 +0100

    Fixes for Spanish name entry screen

--- psrp/ps1jert.sms.asm
+++ psrp/ps1jert.sms.asm
@@ -4381,30 +4381,28 @@ NameEntryLookup:
 .endif
 .if LANGUAGE == "es"
 .db 14
-  NameEntryText  4,  1,  "Introduzca su nombre"
-  NameEntryText  3, 11, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  ;Revisar
+  NameEntryText  6,  1,    "Introduzca su nombre"
+  NameEntryText  3, 11, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   NameEntryText  3, 13, "abcdefghijklmnopqrstuvwxyz"
-  NameEntryText  3, 15, "áéíóúñ"
-  NameEntryText  3, 17, "0123456789"
-  NameEntryText 22, 17,                   ".,-!?‘’"
-  NameEntryText  3, 19, "Atrás"
-  NameEntryText 22, 19,                    "Siguiente"
-  NameEntryText  3, 21, "Espacio"
-  NameEntryText 22, 21,                    "Guardar"
+  NameEntryText  3, 15, "áéíóúñ 0123456789  .,-!?‘’"
+  NameEntryText  3, 17, "Atrás"
+  NameEntryText 20, 17,                  "Siguiente"
+  NameEntryText  3, 19, "Espacio"
+  NameEntryText 22, 19,                    "Guardar"
   NameEntryText  1,  3, "┌─" ; Leave these ones alone...
   NameEntryText  1, 23, "╘═"
   NameEntryText 30,  3, "╖"
   NameEntryText 30, 23, "╝"
 NameEntryLookup:
 .db 4
-  NameEntryMask  3, 19, 6, "B" ; X, Y, length, type (Back)
-  NameEntryMask 22, 19, 7, "N" ; Next
-  NameEntryMask  3, 21, 5, "S" ; Space
-  NameEntryMask 22, 21, 6, "V" ; saVe
+  NameEntryMask  3, 17, 5, "B" ; X, Y, length, type (Back)
+  NameEntryMask 20, 17, 9, "N" ; Next
+  NameEntryMask  3, 19, 7, "S" ; Space
+  NameEntryMask 22, 19, 7, "V" ; saVe
 .define NameEntryMinX 3
 .define NameEntryMaxX 28
 .define NameEntryMinY 11
-.define NameEntryMaxY 21
+.define NameEntryMaxY 19
 .endif
 
 _CursorMemoryInitialValues:
