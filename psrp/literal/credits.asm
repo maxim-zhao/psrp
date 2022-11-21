0857c2b01232821fd81972f4be675689efcf4087
gensakudan <teamgaijinalex@gmail.com>
2022-11-21 13:45:42 +0100

    Literal English translation revision, part 8.

--- psrp/literal/credits.asm
+++ psrp/literal/credits.asm
@@ -1,76 +1,50 @@
 CreditsScreen1: .db 1 ; entry count
   CreditsEntry 13,10,"STAFF"
-CreditsScreen2: .db 5
+CreditsScreen2: .db 3
   CreditsEntry 5,5,"TOTAL"
   CreditsEntry 6,7,"PLANNING"
-  CreditsEntry 17,5,"OSSALE KOHTA"
-  CreditsEntry 17,6,"(KOTARO"
-  CreditsEntry 20,7,   "HAYASHIDA)"
-CreditsScreen3: .db 7
+  CreditsEntry 17,6,"OSSALE KOHTA"
+CreditsScreen3: .db 5
   CreditsEntry 6,6,"STORY BY"
   CreditsEntry 17,6,"APRIL FOOL"
   CreditsEntry 6,14,"SCENARIO"
   CreditsEntry 7,16,"WRITER"
-  CreditsEntry 17,14,"OSSALE KOHTA"
-  CreditsEntry 17,15,"(KOTARO"
-  CreditsEntry 20,16,   "HAYASHIDA)"
-CreditsScreen4: .db 8
+  CreditsEntry 17,15,"OSSALE KOHTA"
+CreditsScreen4: .db 5
   CreditsEntry 4,5,"ASSISTANT"
   CreditsEntry 3,7,"COORDINATORS"
   CreditsEntry 18,6,"FINOS PATA"
-  CreditsEntry 10,10,"OTEGAMI CHIE"
-  CreditsEntry 10,11,"(CHIEKO AOKI)"
-  CreditsEntry 18,14, "GAMER MIKI"
-  CreditsEntry 17,15,"(MIKI"
-  CreditsEntry 21,16,    "MORIMOTO)"
-CreditsScreen5: .db 9
+  CreditsEntry 10,11,"OTEGAMI CHIE"
+  CreditsEntry 18,15, "GAMER MIKI"
+CreditsScreen5: .db 5
   CreditsEntry 3,6,"TOTAL DESIGN"
-  CreditsEntry 18,5, "PHOENIX RIE"
-  CreditsEntry 17,6,"(RIEKO"
-  CreditsEntry 23,7,      "KODAMA)"
+  CreditsEntry 18,6, "PHOENIX RIE"
   CreditsEntry 5,14,"MONSTER"
   CreditsEntry 5,16,"DESIGN"
-  CreditsEntry 17,14,"CHAOTIC KAZ"
-  CreditsEntry 17,15,"(KAZUYUKI"
-  CreditsEntry 22,16,     "SHIBATA)"
-CreditsScreen6: .db 7
+  CreditsEntry 17,15,"CHAOTIC KAZ"
+CreditsScreen6: .db 3
   CreditsEntry 8,6,"DESIGN"
-  CreditsEntry 9,10,"ROCKHY NAO"
-  CreditsEntry 9,11,"(NAOTO"
-  CreditsEntry 15,12,     "OHSHIMA)"
-  CreditsEntry 17,14,"SADAMORIAN"
-  CreditsEntry 17,15,"(KOKI"
-  CreditsEntry 21,16,    "SADAMORI)"
-CreditsScreen7: .db 8
+  CreditsEntry 9,11,"ROCKHY NAO"
+  CreditsEntry 17,15,"SADAMORIAN"
+CreditsScreen7: .db 4
   CreditsEntry 8,6,"DESIGN"
-  CreditsEntry 11,10, "MYAU CHOKO"
-  CreditsEntry 9,11,"(TAKAKO"
-  CreditsEntry 13,12,   "KAWAGUCHI)"
+  CreditsEntry 11,11, "MYAU CHOKO"
   CreditsEntry 8,15,"G CHIE"
-  CreditsEntry 18,14, "YONESAN"
-  CreditsEntry 17,15,"(HITOSHI"
-  CreditsEntry 23,16,      "YONEDA)"
-CreditsScreen8: .db 8
+  CreditsEntry 18,15, "YONESAN"
+CreditsScreen8: .db 4
   CreditsEntry 9,6,"SOUND"
-  CreditsEntry 18,5, "BO"
-  CreditsEntry 17,6,"(TOKUHIKO"
-  CreditsEntry 24,7,       "UWABO)"
+  CreditsEntry 18,6, "BO"
   CreditsEntry 4,15,"SOFT CHECK"
-  CreditsEntry 18,14, "WORKS NISHI"
-  CreditsEntry 17,15,"(AKINORI"
-  CreditsEntry 20,16,   "NISHIYAMA)"
-CreditsScreen9: .db 7
+  CreditsEntry 18,15, "WORKS NISHI"
+CreditsScreen9: .db 5
   CreditsEntry 3,5,"ASSISTANT"
   CreditsEntry 4,7,"PROGRAMMERS"
   CreditsEntry 9,10,"COM BLUE"
-  CreditsEntry 4,14,  "M WAKA"
-  CreditsEntry 2,15,"(MASAHIRO"
-  CreditsEntry 6,16,    "WAKAYAMA)"
+  CreditsEntry 4,15,  "M WAKA"
   CreditsEntry 19,15,"ASI"
-CreditsScreen10: .db 3
+CreditsScreen10: .db 2
   CreditsEntry 2,6,"MAIN PROGRAM"
-  CreditsEntry 17,5,"MUUUU YUJI"
-  CreditsEntry 17,7,"(YUJI NAKA)"
+  CreditsEntry 17,6,"MUUUU YUJI"
 CreditsScreen11: .db 1
   CreditsEntry 9,10,"RETRANSLATION"
 CreditsScreen12: .db 4
@@ -78,11 +52,9 @@ CreditsScreen12: .db 4
   CreditsEntry 18,6,"PAUL JENSEN"
   CreditsEntry 13,10,"SATSU"
   CreditsEntry 18,15,"POPFAN"
-CreditsScreen13: .db 5
-  CreditsEntry 2,6,"LOCALISATION"
-  CreditsEntry 17,6,"FRANK CIFALDI"
-  CreditsEntry 13,10,"CODE"
-  CreditsEntry 4,15,"Z80 GAIDEN"
+CreditsScreen13: .db 3
+  CreditsEntry 6,6,"CODE"
+  CreditsEntry 10,10,"Z80 GAIDEN"
   CreditsEntry 18,15,"MAXIM"
 CreditsScreen14: .db 3
   CreditsEntry 10,10,"PRESENTED BY"
--- psrp/literal/pronouns.asm
+++ psrp/literal/pronouns.asm
@@ -1,3 +1,11 @@
-; No pronouns for literal
+; Values by index:
+; 0 = he/she
+; 1 = his/her
 _PronounsF:
+.dw _PronounShe, _PronounHer
+_PronounShe: String "she"
+_PronounHer: String "her"
 _PronounsM:
+.dw _PronounHe, _PronounHis
+_PronounHe: String "he"
+_PronounHis: String "his"
