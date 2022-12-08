c81034d35256ffb109790e5c5ada5d5763b292f8
gensakudan <teamgaijinalex@gmail.com>
2022-12-08 10:23:01 +0100

    Literal translation tweaks.

--- psrp/literal/lists.asm
+++ psrp/literal/lists.asm
@@ -5,155 +5,155 @@ Lists:
 Items:  
   String " " 
   
-  String "<A> Wood Cane"                    
-  String "<A> Short Sword"                  
-  String "<An> Iron Sword"                  
-  String "<A> Psycho Wand"                  
-  String "<A> Silver Tusk"                  
-  String "<An> Iron Axe"                    
-  String "<A> Titanium Sword"               
-  String "<A> Ceramic Sword"                
-  String "<A> Needle Gun"                   
-  String "<A> Saber Claw"                   
-  String "<A> Heat Gun"                     
-  String "<A> Light Saber"                  
-  String "<A> Laser Gun"                    
-  String "<A> Laconian Sword"               
-  String "<A> Laconian Axe"                 
+  String "<A> Wood Cane"                    	; ウッドケイン
+  String "<A> Short Sword"                  	; ショートソード
+  String "<An> Iron Sword"                  	; アイアンソード
+  String "<A> Psycho Wand"                  	; サイコウオンド
+  String "<A> Silver Tusk"                  	; シルバータスク
+  String "<An> Iron Axe"                    	; アイアンアクス
+  String "<A> Titanium Sword"               	; チタニウムソード
+  String "<A> Ceramic Sword"                	; セラミックソード
+  String "<A> Needle Gun"                   	; ニードルガン
+  String "<A> Saber Claw"                   	; サーベルクロー
+  String "<A> Heat Gun"                     	; ヒートガン
+  String "<A> Light Saber"                  	; ライトセイバー
+  String "<A> Laser Gun"                    	; レーザーガン
+  String "<A> Laconian Sword"               	; ラコニアンソード
+  String "<A> Laconian Axe"                 	; ラコニアンアクス
 
-  String "Leather Clothes"              
-  String "<A> White Mantle"                 
-  String "<A> Light Suit"                   
-  String "<An> Iron Armor"                  
-  String "<A> Spiky Squirrel Fur"           
-  String "<A> Zirconia Mail"                
-  String "<A> Diamond Armor"                
-  String "<A> Laconian Armor"               
-  String "<The> Frad Mantle"                
+  String "Leather Clothes"              		; レザークロス
+  String "<A> White Mantle"                 	; ホワイトマント
+  String "<A> Light Suit"                   	; ライトスーツ
+  String "<An> Iron Armor"                  	; アイアンアーマー
+  String "<A> Spiky Squirrel Fur"           	; トゲリスノケガワ		棘栗鼠の毛皮
+  String "<A> Zirconia Mail"                	; ジルコニアメイル
+  String "<A> Diamond Armor"                	; ダイヤノヨロイ		ダイヤの鎧
+  String "<A> Laconian Armor"               	; ラコニアアーマー
+  String "<The> Frad Mantle"                	; フラードマント
 
-  String "<A> Leather Shield"               
-  String "<An> Iron Shield"                 
-  String "<A> Bronze Shield"                
-  String "<A> Ceramic Shield"               
-  String "<An> Animal Glove"                
-  String "<A> Laser Barrier"                
-  String "<The> Shield of Perseus"          
-  String "<A> Laconian Shield"              
+  String "<A> Leather Shield"               	; レザーシールド
+  String "<An> Iron Shield"                 	; アイアンシールド
+  String "<A> Bronze Shield"                	; ブロンズシールド
+  String "<A> Ceramic Shield"               	; セラミックノタテ		セラミックの盾
+  String "<An> Animal Glove"                	; アニマルグラブ
+  String "<A> Laser Barrier"                	; レーザーバリア
+  String "<The> Shield of Perseus"          	; ペルセウスノタテ		ペルセウスの盾
+  String "<A> Laconian Shield"              	; ラコニアシールド
 
-  String "<The> LandMaster"                 
-  String "<The> FlowMover"                  
-  String "<The> IceDecker"                  
+  String "<The> LandMaster"                 	; ランドマスター
+  String "<The> FlowMover"                  	; フロームーバー
+  String "<The> IceDecker"                  	; アイスデッカー
 
-  String "<A> PelorieMate"                  
-  String "<A> Ruoginin"                     
-  String "<The> Soothe Flute"               
-  String "<A> Searchlight"                  
-  String "<An> Escape Cloth"                
-  String "<A> TranCarpet"                   
-  String "<A> Magic Hat"                    
-  String "<An> Alsuline"                    
-  String "<A> Polymeteral"                  
-  String "<A> Dungeon Key"                  
-  String "<A> Telepathy Ball"               
-  String "<The> Eclipse Torch"              
-  String "<The> Aeroprism" 
-  String "<The> Laerma Berries"             
-  String "Hapsby"                           
-  String "<A> Roadpass"                     
-  String "<A> Passport"                     
-  String "<A> Compass"                      
-  String "<A> Shortcake"                    
-  String "<The> Governor[-General]'s Letter"
-  String "<A> Laconian Pot"                 
-  String "<The> Light Pendant"              
-  String "<The> Carbuncle Eye"              
-  String "<A> GasClear"                     
-  String "Damoa's Crystal"                  
-  String "<A> Master System"                
-  String "<The> Miracle Key"                
-  String "Zillion"                          
-  String "<A> Secret Thing"                 
+  String "<A> PelorieMate"                  	; ペロリーメイト
+  String "<A> Ruoginin"                     	; ルオギニン
+  String "<The> Soothe Flute"               	; スーズフルート
+  String "<A> Searchlight"                  	; サーチライト
+  String "<An> Escape Cloth"                	; エスケープクロス
+  String "<A> TranCarpet"                   	; トランカーペット
+  String "<A> Magic Hat"                    	; マジックハット
+  String "<An> Alsuline"                    	; アルシュリン
+  String "<A> Polymeteral"                  	; ポリメテラール
+  String "<A> Dungeon Key"                  	; ダンジョンキー
+  String "<A> Telepathy Ball"               	; テレパシーボール
+  String "<The> Eclipse Torch"              	; イクリプストーチ
+  String "<The> Aeroprism"						; エアロプリズム
+  String "<The> Laerma Berries"             	; ラエルマベリー
+  String "Hapsby"                           	; ハプスビー
+  String "<A> Roadpass"                     	; ロードパス
+  String "<A> Passport"                     	; パスポート
+  String "<A> Compass"                      	; コンパス
+  String "<A> Shortcake"                    	; ショートケーキ
+  String "<The> Governor[-General]'s Letter"	; ソウトクノテガミ		総督の手紙
+  String "<A> Laconian Pot"                 	; ラコニアンポット
+  String "<A> Light Pendant"              		; ライトペンダント
+  String "<The> Carbuncle Eye"              	; カーバンクルアイ
+  String "<A> GasClear"                     	; ガスクリア
+  String "Damoa's Crystal"                  	; ダモアクリスタル
+  String "<A> Master System"                	; マスターシステム
+  String "<The> Miracle Key"                	; ミラクルキー
+  String "Zillion"                          	; ジリオン
+  String "<A> Secret Thing"                 	; ヒミツノモノ			秘密の物
 
 Names:
-  String "Alisa"                            
-  String "Myau"                             
-  String "Tairon" 
-  String "Lutz"                             
+  String "Alisa"                            	; アリサ
+  String "Myau"                             	; ミャウ
+  String "Tylon" 								; タイロン
+  String "Lutz"                             	; ルツ
 
 Enemies:
   String " " 
-  String "<The> Monster Fly"                
-  String "<The> Green Slime"                
-  String "<The> Wing Eye"                   
-  String "<The> Maneater"                   
-  String "<The> Scorpius"                   
-  String "<The> Giant Naiad"                
-  String "<The> Blue Slime"                 
-  String "<The> Motavian Peasant"           
-  String "<The> Devil Bat"                  
-  String "<The> Killer Plant"               
-  String "<The> Biting Fly"                 
-  String "<The> Motavian Teaser"            
-  String "<The> Herex"                      
-  String "<The> Sandworm"                   
-  String "<The> Motavian Maniac"            
-  String "<The> Gold Lens" 
-  String "<The> Red Slime"                  
-  String "<The> Bat Man"                    
-  String "<The> Horseshoe Crab"             
-  String "<The> Shark King"                 
-  String "<The> Lich"                       
-  String "<The> Tarantula"                  
-  String "<The> Manticort"                  
-  String "<The> Skeleton"                   
-  String "<The> Ant-lion"                   
-  String "<The> Marshes"                    
-  String "<The> Dezorian"                   
-  String "<The> Desert Leech"               
-  String "<The> Cryon"                      
-  String "<The> Big Nose"                   
-  String "<The> Ghoul"                      
-  String "<The> Ammonite" 
-  String "<The> Executor"                   
-  String "<The> Wight"                      
-  String "<The> Skull Soldier"              
-  String "<The> Snail"                      
-  String "<The> Manticore"                  
-  String "<The> Serpent"                    
-  String "<The> Leviathan"                  
-  String "<The> Dorouge"                    
-  String "<The> Octopus"                    
-  String "<The> Mad Stalker"                
-  String "<The> Dezorian Head"              
-  String "<The> Zombie"                     
-  String "<The> Living Dead"                
-  String "<The> Robot Police"               
-  String "<The> Cyborg Mage"                
-  String "<The> Flame Lizard" 
-  String "Tajim"                            
-  String "<The> Gaia"                       
-  String "<The> Machine Guard"              
-  String "<The> Big Eater"                  
-  String "<The> Talos"                      
-  String "<The> Snake Lord"                 
-  String "<The> Death Bearer"               
-  String "<The> Chaos Sorcerer"             
-  String "<The> Centaur"                    
-  String "<The> Ice Man"                    
-  String "<The> Vulcan"                     
-  String "<The> Red Dragon"                 
-  String "<The> Green Dragon"               
-  String "LaShiec"                          
-  String "<The> Mammoth"                    
-  String "<The> King Saber"   
-  String "<The> Dark Marauder"              
-  String "<The> Golem"                      
-  String "Medusa"                           
-  String "<The> Frost Dragon"               
-  String "Dragon Wise"                      
-  String "<The> Gold Drake"                 
-  String "<The> Mad Doctor"                 
-  String "LaShiec"                          
-  String "Dark Force"                       
-  String "Nightmare"                        
+  String "<The> Monster Fly"                	; モンスターフライ
+  String "<The> Green Slime"                	; グリーンスライム
+  String "<The> Wing Eye"                   	; ウイングアイ
+  String "<The> Maneater"                   	; マンイーター
+  String "<The> Scorpirus"                   	; スコーピラス
+  String "<The> Large Naiad"                	; ラージヤーゴ			ラージ水蠆
+  String "<The> Blue Slime"                 	; ブルースライム
+  String "<The> Motavian Peasant"           	; モタビアンノーフ		モタビアン農夫
+  String "<The> Devil Bat"                  	; デビルバット
+  String "<The> Killer Plant"               	; キラープラント
+  String "<The> Biter Fly"	                 	; バイターフライ
+  String "<The> Motavian Evil"	            	; モタビアンイビル
+  String "<The> Herex"                      	; ヘレックス
+  String "<The> Sandworm"                   	; サンドワーム
+  String "<The> Motavian Maniac"            	; モタビアンマニア
+  String "<The> Gold Lens" 						; ゴールドレンズ
+  String "<The> Red Slime"                  	; レッドスライム
+  String "<The> Bat Man"                    	; バットマン
+  String "<The> Horseshoe Crab"             	; カブトガニ			兜蟹
+  String "<The> Sharkin"	                 	; シャーキン
+  String "<The> Lich"                       	; リッチ
+  String "<The> Tarantula"                  	; タランチュラ
+  String "<The> Manticore"                  	; マンチコア
+  String "<The> Skeleton"                   	; スケルトン
+  String "<The> Ant-lion"                   	; アリジゴク			蟻地獄
+  String "<The> Marshes"                    	; マーシーズ
+  String "<The> Dezorian"                   	; デゾリアン
+  String "<The> Desert Leech"               	; デザートリーチ
+  String "<The> Cryon"                      	; クライオン
+  String "<The> Big Nose"                   	; ビッグノーズ
+  String "<The> Ghoul"                      	; グール
+  String "<The> Ammonite" 						; アンモナイト
+  String "<The> Executor"                   	; エグゼキュート
+  String "<The> Wight"                      	; ワイト
+  String "<The> Skull Soldier"              	; スカルソルジャー
+  String "<The> Snail"                      	; マイマイ				まいまい
+  String "<The> Manticort"                  	; マンチコート
+  String "<The> Serpent"                    	; サーペント
+  String "<The> Leviathan"                  	; リバイアサン
+  String "<The> Dorouge"                    	; ビュート
+  String "<The> Octopus"                    	; オクトパス
+  String "<The> Mad Stalker"                	; マッドストーカー
+  String "<The> Dezorian Head"              	; デゾリアンヘッド
+  String "<The> Zombie"                     	; ゾンビ
+  String "<The> Living Dead"                	; リビングデッド
+  String "<The> Robot Police"               	; ロボットポリス
+  String "<The> Cyborg Mage"                	; サイボーグメイジ
+  String "<The> Flame Lizard" 					; フレームリザード
+  String "Tajim"                            	; タジム
+  String "<The> Gaia"                       	; ガイア
+  String "<The> Machine Guarder"              	; マシンガーダー
+  String "<The> Big Eater"                  	; ビッグイーター
+  String "<The> Talos"                      	; タロス
+  String "<The> Snake Lord"                 	; スネークロード
+  String "<The> Death Bearer"               	; デスベアラー
+  String "<The> Chaos Sorcerer"             	; カオスソーサラー
+  String "<The> Centaur"                    	; セントール
+  String "<The> Ice Man"                    	; アイスマン
+  String "<The> Vulcan"                     	; バルカン
+  String "<The> Red Dragon"                 	; レッドドラゴン
+  String "<The> Green Dragon"               	; グリーンドラゴン
+  String "LaShiec"                          	; ラシーク
+  String "<The> Mammoth"                    	; マンモス
+  String "<The> King Saber"   					; キングセイバー
+  String "<The> Dark Marauder"              	; ダークマローダー
+  String "<The> Golem"                      	; ゴーレム
+  String "Medusa"                           	; メデューサ
+  String "<The> Frost Dragon"               	; フロストドラゴン
+  String "Dragon Wise"                      	; ドラゴンワイズ
+  String "<The> Gold Drake"                 	; ゴールドドレイク
+  String "<The> Mad Doctor"                 	; マッドドクター
+  String "LaShiec"                          	; ラシーク
+  String "Dark Falz"	                       	; ダークファルス
+  String "Nightmare"                        	; ナイトメア
 .ends
