34169a11d778a83a2d7ad1268afec25d79b882d8
Maxim <maxim@smspower.org>
2025-07-14 12:48:50 +0100

    Name entry screen: added long vowels, rearranged to fit with more balance

--- psrp/la/name-entry-data.asm
+++ psrp/la/name-entry-data.asm
@@ -1,12 +1,35 @@
 _NameEntryItems:
-.db 11
-  NameEntryText  6,  1, "Nomen tuum inscribe:"
-  NameEntryText  3, 11, "ABCDEFGHIKLMNOPQRSTUVXYZ"
-  NameEntryText  3, 13, "abcdefghiklmnopqrstuvxyz"
-  NameEntryText  3, 15, "0123456789"
-  NameEntryText 21, 15,                   ".,:-!?‘’"
-  NameEntryText  3, 17, "Retro  Proximo  Spatium"
-  NameEntryText 22, 19,                    "Servare"
+;
+;       Nōmen tuum inscrībe:
+;
+;  ╭────────────────────────────╮
+;  │                            │
+;  │                            │
+;  │     _                      │
+;  │                            │
+;  │                            │
+;  │                            │
+;  │                            │
+;  │  ABCDEFGHIKLMNOPQRSTUVXYZ  │
+;  │                            │
+;  │  abcdefghiklmnopqrstuvxyz  │
+;  │                            │
+;  │  āēīōūȳ0123456789.,:-!?‘’  │
+;  │                            │
+;  │  Retrō  Proximō   Spatium  │
+;  │                            │
+;  │                   Servāre  │
+;  │                            │
+;  │                            │
+;  │                            │
+;  ╰────────────────────────────╯
+.db 10
+  NameEntryText  6,  1, "Nōmen tuum inscrībe:"
+  NameEntryText  4, 11, "ABCDEFGHIKLMNOPQRSTUVXYZ"
+  NameEntryText  4, 13, "abcdefghiklmnopqrstuvxyz"
+  NameEntryText  4, 15, "āēīōūȳ0123456789.,:-!?‘’"
+  NameEntryText  4, 17, "Retrō  Proximō   Spatium"
+  NameEntryText 21, 19,                  "Servāre"
   NameEntryText  1,  3, "┌─" ; Leave these ones alone...
   NameEntryText  1, 23, "╘═"
   NameEntryText 30,  3, "╖"
@@ -14,12 +37,12 @@ _NameEntryItems:
 
 _NameEntryLookup:
 .db 4
-  NameEntryMask  3, 17, 5, "B" ; X, Y, length, type (Back)
-  NameEntryMask 10, 17, 7, "N" ; Next
-  NameEntryMask 19, 17, 7, "S" ; Space
-  NameEntryMask 22, 19, 7, "V" ; saVe
+  NameEntryMask  4, 17, 5, "B" ; X, Y, length, type (Back)
+  NameEntryMask 11, 17, 7, "N" ; Next
+  NameEntryMask 21, 17, 7, "S" ; Space
+  NameEntryMask 21, 19, 7, "V" ; saVe
 
-.define NameEntryMinX 3
+.define NameEntryMinX 4
 .define NameEntryMaxX 28
 .define NameEntryMinY 11
 .define NameEntryMaxY 19
